1f9f72cae1a78e64f7a014fa2d077ccb21d69a53
Guilherme <guilherme.mendes@anbetec.com.br>
2024-11-06 09:22:01 -0300

    feat: start developer route

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import DefaultRouter from "./domain/default.router";
 import TaskRouter from "./domain/task/task.router";
+import DeveloperRouter from "./domain/developer/developer.router";
 
 const AppRouter = Router();
-AppRouter.use("/", DefaultRouter)
-.use("/task", TaskRouter)
-
+AppRouter.use("/", DefaultRouter).use("/task", TaskRouter).use("/developer", DeveloperRouter);
 
 export default AppRouter;
